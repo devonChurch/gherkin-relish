16cdf5216f72a2e616229a21b37dc3a0a9294953
Devon Church <notifydevon@gmail.com>
2021-10-25 15:28:49 +1300

    feat(publish): setup semantic realesse for publishing browser utliities

--- packages/browser-utilities/src/index.ts
+++ packages/browser-utilities/src/index.ts
@@ -1,5 +1,7 @@
 console.log("browser utilities!");
 
-export const addNumbers = (values: number[]) => values.reduce((acc, value) => acc + value, 0);
+export const addNumbers = (values: number[]) =>
+  values.reduce((acc, value) => acc + value, 0);
 
-export const multiplyNumbers = (values: number[]) => values.reduce((acc, value) => acc * value, 1);
+export const multiplyNumbers = (values: number[]) =>
+  values.reduce((acc, value) => acc * value, 1);
